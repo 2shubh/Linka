8a10606a3d6ca63854f7efb082febb451cc08a91
Shubham <shubhamgond7678@gmail.com>
2025-08-16 17:56:05 +0530

    create story section

---
+++ client/src/components/StoryModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StoryModal = () => {
+
+
+    const handleMediaUpload=(e)=>{
+
+        const file = e.target.files?.[0]
+    }
+  return (
+    <div></div>
+  )
+}
+
+export default StoryModal

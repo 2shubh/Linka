7ccebd94fd1bab3b6e1a634ce52fd8ab6af6b3ce
Shubham <shubhamgond7678@gmail.com>
2025-08-24 23:21:58 +0530

    email reminder added

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+
+
+export const protect =async (req, res, next) =>{
+try{
+    const {userId} = await req.auth();
+
+    if(!userId){
+        return res.json({
+            success:false,
+            message:"Not authenticated"
+        })
+    }
+    next()
+
+
+}catch(error){
+    return res.json({
+        success:false,
+        messsage:error.message,
+    })
+
+}
+}
